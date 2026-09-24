c480360cf89ff6e4eaf1c234b8c58394c89c8ef0
jagadeeswaran-K007 <jagadees031100@gmail.com>
2026-09-24 20:47:11 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://bulk-mail-app-taupe.vercel.app', // replace local host link
 });
 
 export default API;
